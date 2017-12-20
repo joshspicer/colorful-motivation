daa72bf6c2fdbe662abff7da13523deab085548e
Josh Spicer <josh@JS-MBP.local>
2017-12-19 20:58:30 -0500

    first commit

---
+++ logic.js
@@ -0,0 +1,62 @@
+// Written by Josh Spicer
+// http://joshspicer.com/
+
+//Read it using the storage API
+var arr;
+chrome.storage.sync.get(['month', 'day','year','hour','minute'], function(items) {
+console.log('Information retrieved', items);
+arr = items;
+});
+
+function calculateAge() {
+
+  var birthday = new Date(arr['month'] + " " + arr['day'] +
+                            ", " + arr['year']  + " " +  arr['hour'] +
+                            ":" + arr['minute'] + ":00");
+  var currentTime = new Date();
+  var difference = currentTime - birthday;
+  // How old am I in years (with a decimal)
+  return (difference / (1000*60*60*24)) / 365.2422;
+}
+
+function displayBirthday() {
+  if (arr) {
+  return arr['month'] + " " + arr['day'] + ", " +
+         arr['year'] + " at " + arr['hour'] + ":" + arr['minute'];
+  } else {
+  return "January 1, 2000";
+  }
+
+}
+
+function firstHalf() {
+  var age = calculateAge();
+  return Math.floor(age);
+}
+
+function secondHalf() {
+  var age = calculateAge();
+  return (age % 1).toFixed(10).substring(2);
+}
+
+
+window.setInterval(function(){
+  document.getElementById("firstHalf").innerHTML = firstHalf();
+}, 100);
+
+window.setInterval(function(){
+  document.getElementById("secondHalf").innerHTML = secondHalf();
+}, 100);
+
+window.setInterval(function(){
+  document.getElementById("colorInfo").innerHTML = "#" + secondHalf().substring(1,7);
+}, 100);
+
+window.setInterval(function(){
+  // Sets background color based on my age!
+  document.body.style.background = "#" + secondHalf().substring(1,7);
+}, 100);
+
+window.setInterval(function(){
+  document.getElementById("birthday").innerHTML = displayBirthday();
+}, 100);
---
+++ options.js
@@ -0,0 +1,54 @@
+// http://joshspicer.com/
+
+// Saves options to chrome.storage
+function save_options() {
+  // var color = document.getElementById('color').value;
+  // var likesColor = document.getElementById('like').checked;
+
+  var m = document.getElementById('m').value;
+  var d = document.getElementById('d').value;
+  var y = document.getElementById('y').value;
+  var hr = document.getElementById('hr').value;
+  var min = document.getElementById('min').value;
+
+  chrome.storage.sync.set({
+    month: m,
+    day: d,
+    year: y,
+    hour: hr,
+    minute: min
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+
+  window.location = "./index.html"; //TODO maybe change this?
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default values.
+  chrome.storage.sync.get({
+    month: "jan",
+    day: 1,
+    year:1990,
+    hour: 6,
+    minute: 30
+  }, function(items) {
+    document.getElementById('m').value = items.month;
+    document.getElementById('d').value = items.day;
+    document.getElementById('y').value = items.year;
+    document.getElementById('hr').value = items.hour;
+    document.getElementById('min').value = items.minute;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+
+document.getElementById('save').addEventListener('click',
+    save_options);
